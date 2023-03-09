554406d1ed0df01dc303a78739ef27012c8bb78f
cristal20777 <cristal20777@yandex.ru>
2023-03-09 16:25:52 +0300

    Задание 7

--- src/test/java/app/manager/ApplicationManager.java
+++ src/test/java/app/manager/ApplicationManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import model.ContactData;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +17,20 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private JavascriptExecutor js;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+
+    if (browser == BrowserType.CHROME) {
+      wd= new ChromeDriver();
+    } else if (browser == BrowserType. FIREFOX){
+      wd= new FirefoxDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
