54a2f3ad9393fac8dfebb093036b029f0540ca55
cristal20777 <cristal20777@yandex.ru>
2023-03-23 16:30:48 +0300

    Задание 8 попытка 3

--- src/test/java/app/manager/ContactHelper.java
+++ src/test/java/app/manager/ContactHelper.java
@@ -28,7 +28,7 @@ public class ContactHelper extends HelperBase {
     type(By.name("email"), contactData.getEmail());
 
       if (creation) {
-        new Select(wd.findElement(By.name("new_group"))).selectByIndex(0);
+        new Select(wd.findElement(By.name("new_group"))).selectByIndex(1);
       } else {
         Assert.assertFalse(isElementPresent(By.name("new_group")));
 
--- src/test/java/tests/ContactCreationTests.java
+++ src/test/java/tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ public class ContactCreationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup (new GroupData("test1",null,null));
     }
-    app.getcontactHelper().createContact(new ContactData("Михаил", "Голик", "Москва, Сретенский бульвар 17-64", "89600267885", "golikmisha1@mail.ru","gfh"));
+    app.getcontactHelper().createContact(new ContactData("Михаил", "Голик", "Москва, Сретенский бульвар 17-64", "89600267885", "golikmisha1@mail.ru","test1"));
 
     app.getNavigationHelper().gotoHomePage();
   }
