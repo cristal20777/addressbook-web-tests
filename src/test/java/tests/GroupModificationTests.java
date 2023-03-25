429e720ce8c8ca6eed277ea8e533d0818d451d8a
cristal20777 <cristal20777@yandex.ru>
2023-03-25 15:38:55 +0300

    4.5 сравниваем размеры списков

--- src/test/java/tests/GroupModificationTests.java
+++ src/test/java/tests/GroupModificationTests.java
@@ -4,22 +4,22 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification(){
-
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup (new GroupData("test1",null,null));
     }
-    app.getGroupHelper().selectedGroup(before-1);
+    List<GroupData> before=app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectedGroup(before.size()-1);
     app.getGroupHelper().initgroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("gfh", "dhth", "dhdth"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
-
+    List<GroupData> after =app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
