15350dfc69b8bb2683f1a6e7f1632257fc1c8631
cristal20777 <cristal20777@yandex.ru>
2023-04-03 12:33:24 +0300

    задание 10

--- src/test/java/tests/ContactCreationTests.java
+++ src/test/java/tests/ContactCreationTests.java
@@ -2,16 +2,8 @@ package tests;
 
 import model.Contacts;
 import model.GroupData;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import model.ContactData;
-
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
