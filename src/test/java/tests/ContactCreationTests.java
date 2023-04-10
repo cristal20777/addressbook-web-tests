629c48d77891a4a8c0194048077a0847a0c1b266
cristal20777 <cristal20777@yandex.ru>
2023-04-10 07:23:30 +0300

    6.10

--- src/test/java/tests/ContactCreationTests.java
+++ src/test/java/tests/ContactCreationTests.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 
 public class ContactCreationTests extends TestBase {
